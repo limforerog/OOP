db449aa4bc164635fe4fad6cf5c5faf5b3ab328d
User1001 <linaforerog1@gmail.com>
2025-04-21 21:49:51 -0500

    Update Operations.java

--- Corte 1/Tarea2/Operations.java
+++ Corte 1/Tarea2/Operations.java
@@ -1,29 +1,45 @@
 public class Operations {
-    private Data data; // Options se almacena en data
-    
-    //Brindar constructores
-    public Operations() {
-        data = new Data(); // inicializo Data
+    private int num1;
+    private int num2;
+
+    // Constructor
+    public Operations(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
     }
 
-    // Operaciones
-    public void sumar(int a, int b) {
-        data.setResultado(a + b); // uso data para almacenar el resultado
+    // Getter para num1
+    public int getNum1() {
+        return num1;
     }
 
-    public void restar(int a, int b) {
-        data.setResultado(a - b);//igual aqui 
+    // Setter para num1
+    public void setNum1(int num1) {
+        this.num1 = num1;
     }
 
-    public void multiplicar(int a, int b) {
-        data.setResultado(a * b);
+    // Getter para num2
+    public int getNum2() {
+        return num2;
     }
-   // public void potencia(int base, int exponente) {
-       // data.setResultado((exponente == 0 ? 1 : base * potencia(base, exponente - 1));
-   // }
 
-    // Resultado 
-    public int getResultado() {
-        return data.getResultado();
+    // Setter para num2
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    // Método para sumar
+    public int sumar() {
+        return num1 + num2;
+    }
+
+    // Método para restar
+    public int restar() {
+        return num1 - num2;
+    }
+
+    // Método para multiplicar
+    public int multiplicar() {
+        return num1 * num2;
     }
 }
