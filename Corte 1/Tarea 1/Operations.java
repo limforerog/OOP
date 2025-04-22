79ed37bcf4a711272f481cf0b28a294b9d90e572
User1001 <linaforerog1@gmail.com>
2025-04-21 21:43:22 -0500

    Update Operations.java

--- Corte 1/Tarea 1/Operations.java
+++ Corte 1/Tarea 1/Operations.java
@@ -1,12 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author LINA FORERO
- */
 public class Operations {
     
+    public int a,b,r;
+  // Métodos para operaciones matemáticas
+    public int sumar(int a, int b) {
+        r = a + b;
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        r = a - b;
+        return a - b;   
+    }
+
+    public int multiplicar(int a, int b) {
+        r = a * b;
+        return a * b;   
+    }
+    public int potencia (int base, int exponente){
+        r = exponente == 0 ? 1: base*potencia(base,exponente-1);
+        return exponente == 0 ? 1: base*potencia(base,exponente-1);
+    }  
 }
